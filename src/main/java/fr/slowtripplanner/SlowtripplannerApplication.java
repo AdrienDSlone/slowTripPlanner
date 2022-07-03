7694c5a0f8416e05522e62c180647d12f350d8b6
Adrien <amh.marceau@gmail.com>
2022-07-03 18:34:42 +0200

    config : spring : Spring boot devtools + Spring data JPA + Spring Web, pom.xml : mysql connector + hibernate validator

---
+++ src/main/java/fr/slowtripplanner/SlowtripplannerApplication.java
@@ -0,0 +1,13 @@
+package fr.slowtripplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowtripplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowtripplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/slowtripplanner/SlowtripplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.slowtripplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowtripplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
